7ea2194051ac0713ad90c70c6f6fb1b7ecfa8eab
Administrator <Administrator@PC193>
2020-06-19 16:12:04 +0700

    baitap

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -8,6 +8,7 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        public int Id { get; set; }
         [Requied]
         public string Place { get; set; }
         [Requied]
@@ -20,6 +21,11 @@ namespace BigSchool.ViewModels
         [Requied]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+        public string Heading { get; set; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
